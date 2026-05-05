d49787152ec2c73844679db30f7d88b4e3e717bf
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-05 13:31:15 -0500

    Update browse.js

--- pages/browse.js
+++ pages/browse.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useMemo, useState, useEffect } from "react";
-import motorGradersTaxonomy from "../data/taxonomy-motor-graders.json";const STAGING = "https://staging.ironxchange.com";
+import motorGradersTaxonomy from "../data/taxonomy-motor-graders.json";
+
+const STAGING = "https://staging.ironxchange.com";
 const BRAND_YELLOW = "#FFC400";
 
 const categories = [
