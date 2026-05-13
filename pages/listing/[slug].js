b88c4fd32116b2dfe8153b1a61fe307052a282f4
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-13 16:26:14 -0500

    Update [slug].js

--- pages/listing/[slug].js
+++ pages/listing/[slug].js
@@ -1,9 +1,45 @@
 export default function ListingPage() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>2020 DEERE 872GP</h1>
-      <h2>$179,000</h2>
-      <p>3,875 hrs · Colorado City, TX</p>
+     <div
+  style={{
+    display: "flex",
+    alignItems: "baseline",
+    gap: "20px",
+    flexWrap: "wrap"
+  }}
+>
+  <h1
+    style={{
+      margin: 0,
+      fontSize: "52px",
+      fontWeight: "900",
+      lineHeight: 1
+    }}
+  >
+    2020 DEERE 872GP
+  </h1>
+
+  <div
+    style={{
+      fontSize: "42px",
+      fontWeight: "700",
+      color: "#111"
+    }}
+  >
+    $179,000
+  </div>
+</div>
+
+<p
+  style={{
+    marginTop: "14px",
+    fontSize: "20px",
+    color: "#666"
+  }}
+>
+  3,875 hrs · Colorado City, TX
+</p>
 
       <img
         src="/images/2020-Deere-772GP.jpg"
