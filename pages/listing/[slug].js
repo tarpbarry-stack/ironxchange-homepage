33d61532e786cff380234d48c4822991f4bcbc37
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-14 01:11:43 -0500

    Update [slug].js

--- pages/listing/[slug].js
+++ pages/listing/[slug].js
@@ -280,11 +280,11 @@ function lightboxNext() {
 
            <div className="right-stack">
   <div className="mini-tool-tab">
-    <button>↗</button>
-    <button>👓</button>
-    <a href="/browse">Results</a>
-    <button>Prev</button>
-    <button>Next</button>
+   <button>↗</button>
+<button>👓</button>
+<a href="/browse">← Results</a>
+<button>← Prev</button>
+<button>Next →</button>
   </div>
 
   <div className="panel">
