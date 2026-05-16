cec40db6b167058bc773f9e6b559104e4a64296e
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-16 11:02:35 -0500

    Update [slug].js

--- pages/listing/[slug].js
+++ pages/listing/[slug].js
@@ -220,7 +220,7 @@ function lightboxNext() {
     <i className="fa-regular fa-star"></i>
   </a>
 
-  <a href={`${STAGING}/login`} className="login-icon" aria-label="Login">
+  <a href="/login" className="login-icon" aria-label="Login">
     <i className="fa-regular fa-user"></i>
   </a>
 </div>
