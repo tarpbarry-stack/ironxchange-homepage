c077264084de28dd4201deecd1c924adc5492ed3
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-16 15:50:45 -0500

    Create social-wheel.js

---
+++ pages/ixi/social-wheel.js
@@ -0,0 +1,8 @@
+export default function SOCIALWHEEL() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#111", color: "#fff", padding: "40px" }}>
+      <h1>SOCIALWHEEL</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
