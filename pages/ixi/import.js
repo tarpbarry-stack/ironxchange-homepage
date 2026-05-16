e8b99bc657ba0a47b0bb71fa3cad44eeee46dc14
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-16 15:52:16 -0500

    Create import.js

---
+++ pages/ixi/import.js
@@ -0,0 +1,8 @@
+export default function IMPORT() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#111", color: "#fff", padding: "40px" }}>
+      <h1>IMPORT</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
