9b81c53266a9ecf811a234727f69b6f0de6ca604
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-16 15:47:06 -0500

    Create dealer-graph.js

---
+++ pages/ixi/dealer-graph.js
@@ -0,0 +1,8 @@
+export default function DealerGraph() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#111", color: "#fff", padding: "40px" }}>
+      <h1>Dealer Graph</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
