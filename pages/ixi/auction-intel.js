19a26353d8b802d3dd55df004ff9f3eb30a00994
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-16 15:51:32 -0500

    Create auction-intel.js

---
+++ pages/ixi/auction-intel.js
@@ -0,0 +1,8 @@
+export default function AUCTIONINTEL() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#111", color: "#fff", padding: "40px" }}>
+      <h1>AUCTIONINTEL</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
