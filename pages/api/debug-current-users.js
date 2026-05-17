d0325ee23383b6c9dca2d447ffe360fd3354aae9
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-17 11:11:24 -0500

    Update debug-current-users.js

--- pages/api/debug-current-users.js
+++ pages/api/debug-current-users.js
@@ -1,6 +1,24 @@
+import SharetribeSdk from "sharetribe-flex-sdk";
+
 export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: "debug-current-user route is alive"
-  });
+  try {
+    const sdk = SharetribeSdk.createInstance({
+      clientId: process.env.NEXT_PUBLIC_SHARETRIBE_CLIENT_ID,
+      clientSecret: process.env.SHARETRIBE_CLIENT_SECRET,
+    });
+
+    const response = await sdk.currentUser.show({
+      include: ["profileImage"],
+    });
+
+    return res.status(200).json({
+      ok: true,
+      data: response.data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      error: error?.message || String(error),
+    });
+  }
 }
