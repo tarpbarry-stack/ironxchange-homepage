42cb8ac8d19f3262b2b7d052121cf79f311aeaa5
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-18 00:04:38 -0500

    Update browse.js

--- pages/browse.js
+++ pages/browse.js
@@ -1350,10 +1350,9 @@ if (category === "BACKHOE LOADERS") {
   </a>
 
  <div className="nav-links">
-  <a href={`${STAGING}/l/new`} className="yellow-link">
-    POST FREE
-  </a>
-
+  <a href="/post-free" className="yellow-link">
+  POST FREE
+</a>
   <a href="/saved" className="login-icon" aria-label="Saved Listings">
     <i className="fa-regular fa-star"></i>
   </a>
