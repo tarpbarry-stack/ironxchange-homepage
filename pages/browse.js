629b533b3252fd98586e3843987ae776b83c56f6
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-21 11:04:56 -0500

    Update browse.js

--- pages/browse.js
+++ pages/browse.js
@@ -2040,10 +2040,9 @@ style={{
 
 .cards {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 22px;
 }
-
 .card {
   position: relative;
   text-decoration: none;
