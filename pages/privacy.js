d79e92a98c1fe18ddb66a56bc9c85cbdfc44c42e
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-26 13:22:25 -0500

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -6,9 +6,14 @@ export default function PrivacyPage() {
   return (
     <>
       <Head>
-        <title>Privacy Policy | IronXchange</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+  <title>Privacy Policy | IronXchange</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    rel="stylesheet"
+  />
+</Head>
 
       <main>
         <Navbar />
