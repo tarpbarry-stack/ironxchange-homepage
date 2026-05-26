618455273c51221db31353072c3d1fbb45e0f9ad
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-26 13:20:09 -0500

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -6,9 +6,14 @@ export default function TermsPage() {
   return (
     <>
       <Head>
-        <title>Terms of Service | IronXchange</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+  <title>Terms of Service | IronXchange</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    rel="stylesheet"
+  />
+</Head>
 
       <main>
         <Navbar />
