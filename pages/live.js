b6bfb38d2fa8597b7681111b10f26d67548fbae6
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-28 11:02:08 -0500

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1566,15 +1566,13 @@ onClick={async () => {
               </div>
 
               <div className="listing-preview-card">
-               <div
+              <div
   className="preview-photo"
+  style={{
+    backgroundImage: `url(${heroPhoto || "/images/hero-equipment-yard.jpg"})`
+  }}
   onClick={() => changeActivePhoto(1)}
 >
-  <img
-    src={heroPhoto || "/images/hero-equipment-yard.jpg"}
-    alt={title || "Machine"}
-    className="preview-photo-img"
-  />
                   {photoItems.length > 1 ? (
                     <>
                       <button
