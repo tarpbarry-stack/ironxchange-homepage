5ac9034e08f069431045ee2413dab2b8a552bb0b
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-29 14:45:50 -0500

    Create configCategories.js

---
+++ config/configCategories.js
@@ -0,0 +1,9 @@
+// IronXchange V12 master Sharetribe-compatible category tree.
+// This file will be generated from our latest taxonomy files.
+// Shape: Category → Make → Model.
+
+const categoriesConfig = {
+  categories: []
+};
+
+export default categoriesConfig;
