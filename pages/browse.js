5ba4abca6d4b97ba14b59b12e04a0f98849a729f
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-30 01:09:03 -0500

    Update browse.js

--- pages/browse.js
+++ pages/browse.js
@@ -9,6 +9,7 @@ import {
 } from "../lib/savedListings";
 
 import {
+  getV12CategoryNames,
   getV12Makes,
   getV12Models
 } from "../lib/v12TaxonomyAdapter";
@@ -23,32 +24,7 @@ const BRAND_YELLOW = "#FFC400";
 
 const categories = [
   "ALL CATEGORIES",
-  "AERIAL EQUIPMENT",
-  "AGGREGATE",
-  "AGRICULTURE HARVESTERS",
-  "AGRICULTURE TRACTORS",
-  "ASPHALT EQUIPMENT",
-  "BACKHOE LOADERS",
-  "COMPACTION/ROLLERS",
-  "CRANES",
-  "CRAWLER CARRIERS / LOADER",
-  "DOZERS",
-  "DRILLS & PILING",
-  "DUMP TRUCKS - ARTIC/RIGID",
-  "EXCAVATORS",
-  "FORKLIFTS",
-  "MOTOR GRADERS",
-  "SCRAPER",
-  "SKID STEER/CTL",
-  "TELEHANDLERS",
-  "TRENCHERS/PLOWS",
-  "TRAILERS",
-  "TRUCKS",
-  "WHEEL LOADERS",
-  "ATTACHMENTS / PARTS",
-  "OTHER SPECIALTY",
-  "SUPPORT EQUIPMENT",
-  "UTILITY CARTS"
+  ...getV12CategoryNames()
 ];
 
 function formatKeywordLabel(keyword = "") {
