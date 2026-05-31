9018c2ba90726799d61f114f600a6e2d776cda26
tarpbarry-stack <tarpbarry@gmail.com>
2026-05-30 20:45:50 -0500

    Update ListingCard.js

--- components/ListingCard.js
+++ components/ListingCard.js
@@ -578,7 +578,7 @@ return (
   height: 13px;
   min-height: 13px;
   max-height: 13px;
-  margin: 0px -13px -14px;
+  margin: 4px -13px -14px;
 
   display: grid;
   grid-template-columns: repeat(5, 1fr);
