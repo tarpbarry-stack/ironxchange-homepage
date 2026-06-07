6e1bcc0cd47b47cb60c9964e5fb01e1645a56883
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-06 23:59:25 -0500

    Create IXIMachineRail.js

---
+++ components/IXIMachineRail.js
@@ -0,0 +1,23 @@
+// IXI Machine Rail
+// Card-level machine control strip.
+// One half of IXI Machine Controls™.
+// The other half is IXIEnvironmentRail.
+
+export default function IXIMachineRail({
+  listing,
+  saved,
+  boardColor,
+  boardOutline,
+  onSendFront,
+  onSendBack,
+  onCycleColor,
+  onCycleOutline,
+  onEndRelationship,
+  onToggleSaved
+}) {
+  return (
+    <div className="board-command-rail">
+      {/* rail JSX moves here next */}
+    </div>
+  );
+}
