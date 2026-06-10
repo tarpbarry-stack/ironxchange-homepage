9fc8411fe24bba050891145eb2f6a02eb0413621
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-10 13:58:17 -0500

    Adjust vertical translation for theater stack slots

--- pages/theater.js
+++ pages/theater.js
@@ -1072,7 +1072,7 @@ margin-top: -55px;
 }
 
 .theater-stack-grid .theater-stack-slot:nth-child(-n+3) {
-  transform: translateY(50px);
+  transform: translateY(30px);
 }
 
 .theater-stack-grid .theater-stack-slot:nth-child(n+4) {
