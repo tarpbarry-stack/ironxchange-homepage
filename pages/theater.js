7724838277e678aedca0cde8b8e58e5aa749f57a
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-10 12:52:07 -0500

    Update theater.js

--- pages/theater.js
+++ pages/theater.js
@@ -1072,11 +1072,11 @@ margin-top: -55px;
 }
 
 .theater-stack-grid .theater-stack-slot:nth-child(-n+3) {
-  transform: translateY(20px);
+  transform: translateY(30px);
 }
 
 .theater-stack-grid .theater-stack-slot:nth-child(n+4) {
-  transform: translateY(20px);
+  transform: translateY(30px);
 }
 
 .theater-stack-slot {
