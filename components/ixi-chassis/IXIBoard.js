1f6358749375ede87a879da07942bdcc65296796
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-13 12:44:57 -0500

    Update IXIBoard.js

--- components/ixi-chassis/IXIBoard.js
+++ components/ixi-chassis/IXIBoard.js
@@ -1,1 +1,69 @@
+import { SortableContext } from "@dnd-kit/sortable";
+import { rectSortingStrategy } from "@dnd-kit/sortable";
 
+import ListingCard from "../ListingCard";
+
+export default function IXIBoard({
+  items = [],
+  getListingId,
+  savedIds = [],
+  ixiCardState = {},
+  IXISortableMachineCard,
+  toggleSave,
+  updateIxiCardState,
+  sendListingToFront,
+  sendListingToBack,
+  armedDestination,
+  sendMachineToArmedDestination,
+  draggingListingId,
+  ghostListingId
+}) {
+  return (
+    <SortableContext
+      id="board"
+      items={items.map(item => String(getListingId(item)))}
+      strategy={rectSortingStrategy}
+    >
+      {items.map(item => {
+        const id = String(getListingId(item));
+
+        return (
+          <IXISortableMachineCard
+            key={id}
+            id={id}
+            containerId="board"
+            className="ixi-board-sortable-card"
+          >
+            {({ dragHandleProps }) => (
+              <ListingCard
+                listing={item}
+                saved={savedIds.includes(id)}
+                onToggleSaved={() => toggleSave(item)}
+                from="saved"
+                ixiState={
+                  ixiCardState[id] || {
+                    color: "none",
+                    outline: 1
+                  }
+                }
+                onIxiStateChange={updateIxiCardState}
+                onSendFront={sendListingToFront}
+                onSendBack={sendListingToBack}
+                armedDestination={armedDestination}
+                onSendToArmedDestination={sendMachineToArmedDestination}
+                isBoardDraggingCard={
+                  String(id) === String(draggingListingId)
+                }
+                isGhostTarget={
+                  String(id) === String(ghostListingId)
+                }
+                useDndDrag={false}
+                dragHandleProps={dragHandleProps}
+              />
+            )}
+          </IXISortableMachineCard>
+        );
+      })}
+    </SortableContext>
+  );
+}
