eb222a0fd2a6831aa8429494e2e20fda8dba0dc6
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-13 16:17:12 -0500

    Update IXIWorkspaceEngine.js

--- components/ixi-chassis/IXIWorkspaceEngine.js
+++ components/ixi-chassis/IXIWorkspaceEngine.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 export default function IXIWorkspaceEngine({
   children
 }) {
-  return children;
+  const [leftPocketMode, setLeftPocketMode] = useState("peek");
+  const [rightPocketMode, setRightPocketMode] = useState("peek");
+  const [leftPocket2Mode, setLeftPocket2Mode] = useState("peek");
+  const [rightPocket2Mode, setRightPocket2Mode] = useState("peek");
+
+  return children({
+    leftPocketMode,
+    setLeftPocketMode,
+    rightPocketMode,
+    setRightPocketMode,
+    leftPocket2Mode,
+    setLeftPocket2Mode,
+    rightPocket2Mode,
+    setRightPocket2Mode
+  });
 }
