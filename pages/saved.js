c7d58a8f5606223161881164862562e0ed7a9a90
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-13 01:56:35 -0500

    Update saved.js

--- pages/saved.js
+++ pages/saved.js
@@ -1971,16 +1971,18 @@ onClick={(e) => {
 {activeStackSendMenu === stackKey && (
   <div className="active-stack-send-menu">
     <button
-      type="button"
-      className="stack-send-option"
-      data-label="L1"
-      onClick={() =>
-        moveActiveStackToContainer(
-          stackKey,
-          "pocketLeft"
-        )
-      }
-    />
+  type="button"
+  className={`stack-pocket-power top-left ${
+    armedDestination === "pocketLeft"
+      ? "destination-armed"
+      : ""
+  }`}
+  data-label="L1"
+  title="Arm L1"
+  onClick={() =>
+    toggleArmedDestination("pocketLeft")
+  }
+/>
 
     <button
       type="button"
