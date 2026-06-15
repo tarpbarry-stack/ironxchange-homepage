c0366015a00909c1698dcfdc0feacdebe3df0bdb
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-15 17:35:08 -0500

    Update IXIBoard.js

--- components/ixi-chassis/IXIBoard.js
+++ components/ixi-chassis/IXIBoard.js
@@ -20,7 +20,8 @@ export default function IXIBoard({
   sendMachineToArmedDestination,
   draggingListingId,
   ghostListingId,
-  getSellerListingCardProps
+  getSellerListingCardProps,
+  cardScaleMode = "xl"
 }) {
   return (
     <SortableContext
