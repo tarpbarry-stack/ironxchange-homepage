ed55cff12a23b409f238cb86b33d8769b267bd25
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 13:01:14 -0500

    Add sanitizeWorkspaceContainers function
    Added a new function to sanitize workspace containers by filtering valid machine IDs and ensuring no duplicates.

--- components/ixi-chassis/IXIWorkspacePersistenceEngine.js
+++ components/ixi-chassis/IXIWorkspacePersistenceEngine.js
@@ -13,8 +13,43 @@ function createEmptyWorkspaceContainers() {
   };
 }
 
+function sanitizeWorkspaceContainers(
+  savedContainers,
+  validMachineIds
+) {
+  const valid = new Set(
+    validMachineIds.map(id => String(id))
+  );
+
+  const clean = createEmptyWorkspaceContainers();
+  const seen = new Set();
+
+  Object.keys(clean).forEach(containerKey => {
+    (savedContainers?.[containerKey] || []).forEach(id => {
+      const sid = String(id);
+
+      if (valid.has(sid) && !seen.has(sid)) {
+        clean[containerKey].push(sid);
+        seen.add(sid);
+      }
+    });
+  });
+
+  validMachineIds.forEach(id => {
+    const sid = String(id);
+
+    if (!seen.has(sid)) {
+      clean.board.push(sid);
+      seen.add(sid);
+    }
+  });
+
+  return clean;
+}
+
 export {
   IXI_WORKSPACE_SETTINGS_ID,
   IXI_WORKSPACE_LAYOUT_ID,
-  createEmptyWorkspaceContainers
+  createEmptyWorkspaceContainers,
+  sanitizeWorkspaceContainers
 };
