238330203aad2cf60d948b571a7b6d941157cd6c
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 17:43:47 -0500

    Update saved.js

--- pages/saved.js
+++ pages/saved.js
@@ -127,57 +127,6 @@ function WorkspaceDropPad({
   );
 }
 
-function IXISortableMachineCard({
-  id,
-  containerId,
-  className,
-  style: externalStyle,
-  children
-}) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    setActivatorNodeRef,
-    transform,
-    transition,
-    isDragging
-  } = useSortable({
-    id: String(id),
-    data: {
-      type: "machine",
-      containerId
-    }
-  });
-
-  const style = {
-  ...(externalStyle || {}),
-  transform: CSS.Transform.toString(transform),
-  transition,
-  opacity: isDragging ? 0 : 1,
-  zIndex: isDragging ? 9999 : externalStyle?.zIndex
-};
-
-  return (
-   <div
-  ref={setNodeRef}
-  className={className}
-  style={style}
-  data-ixi-sortable-card={String(id)}
-  data-ixi-container={containerId}
->
-      {children({
-        dragHandleProps: {
-          ref: setActivatorNodeRef,
-          ...attributes,
-          ...listeners
-        },
-        isDragging
-      })}
-    </div>
-  );
-}
-
 export default function SavedListings() {
   const [listings, setListings] = useState([]);
 
