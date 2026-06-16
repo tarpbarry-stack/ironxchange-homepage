3354ca700d6730c136ebade4d3ba676a37268f39
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 12:57:58 -0500

    Create IXIWorkspacePersistenceEngine.js

---
+++ components/ixi-chassis/IXIWorkspacePersistenceEngine.js
@@ -0,0 +1,7 @@
+const IXI_WORKSPACE_SETTINGS_ID = "__workspaceSettings";
+const IXI_WORKSPACE_LAYOUT_ID = "__workspaceLayout";
+
+export {
+  IXI_WORKSPACE_SETTINGS_ID,
+  IXI_WORKSPACE_LAYOUT_ID
+};
