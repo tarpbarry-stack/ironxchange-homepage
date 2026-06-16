239a220a0adcf3a02a41bc318b6ef2cca4b70588
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 17:22:13 -0500

    Enhance IXIActiveStackZone with additional props
    Added multiple props to IXIActiveStackZone component.

--- components/ixi-chassis/IXIActiveStackZone.js
+++ components/ixi-chassis/IXIActiveStackZone.js
@@ -1,5 +1,31 @@
 import IXIActiveStack from "./IXIActiveStack";
 
-export default function IXIActiveStackZone() {
+export default function IXIActiveStackZone({
+  WorkspaceDropZone,
+  activeStacksOpen = {},
+  activeStackHover = "",
+  machineContainers = {},
+  armedDestination,
+  toggleArmedDestination,
+  toggleActiveStack,
+  toggleActiveStackLayout,
+  moveActiveStackToContainer,
+  sendActiveStackToTheater,
+  activeStackSendMenu,
+  setActiveStackSendMenu,
+  activeStackLayouts = {},
+  getListingById,
+  getListingId,
+  savedIds = [],
+  ixiCardState = {},
+  IXISortableMachineCard,
+  toggleSave,
+  updateIxiCardState,
+  cycleMachineFace,
+  sendListingToFront,
+  sendListingToBack,
+  sendMachineToArmedDestination,
+  cardScaleMode = "xl"
+}) {
   return null;
 }
