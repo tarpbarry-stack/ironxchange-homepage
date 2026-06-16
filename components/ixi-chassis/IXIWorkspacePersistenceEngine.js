9612df3c733d353bb0e5c8f5afa77355c7b6533e
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 12:59:20 -0500

    Update IXIWorkspacePersistenceEngine.js

--- components/ixi-chassis/IXIWorkspacePersistenceEngine.js
+++ components/ixi-chassis/IXIWorkspacePersistenceEngine.js
@@ -1,7 +1,20 @@
 const IXI_WORKSPACE_SETTINGS_ID = "__workspaceSettings";
 const IXI_WORKSPACE_LAYOUT_ID = "__workspaceLayout";
 
+function createEmptyWorkspaceContainers() {
+  return {
+    board: [],
+    stackTop: [],
+    stackBottom: [],
+    pocketLeft: [],
+    pocketRight: [],
+    pocketLeft2: [],
+    pocketRight2: []
+  };
+}
+
 export {
   IXI_WORKSPACE_SETTINGS_ID,
-  IXI_WORKSPACE_LAYOUT_ID
+  IXI_WORKSPACE_LAYOUT_ID,
+  createEmptyWorkspaceContainers
 };
