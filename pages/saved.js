fc60dfad8ae8c577fa7912a403e56ced1df7f73c
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-16 11:17:11 -0500

    Delete commented-out useEffect in saved.js
    Removed unused useEffect for loading workspace layout.

--- pages/saved.js
+++ pages/saved.js
@@ -487,30 +487,6 @@ const containerStateKey = useMemo(() => {
     })
     .join("|");
 }, [workspaceListings, ixiCardState]);
-
-/*
-useEffect(() => {
-  if (!loadedWorkspaceLayout) return;
-
-  if (loadedWorkspaceLayout.machineContainers) {
-    setMachineContainers(
-      loadedWorkspaceLayout.machineContainers
-    );
-  }
-
-  if (loadedWorkspaceLayout.activeStackLayouts) {
-    setActiveStackLayouts(
-      loadedWorkspaceLayout.activeStackLayouts
-    );
-  }
-
-  if (loadedWorkspaceLayout.activeStacksOpen) {
-    setActiveStacksOpen(
-      loadedWorkspaceLayout.activeStacksOpen
-    );
-  }
-}, [loadedWorkspaceLayout]);
-*/
   
 useEffect(() => {
   if (!workspaceListings.length) return;
