d37b796715a737b874f7e631bd0877c4af4a0329
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-22 23:02:38 -0500

    Update theater.js

--- pages/theater.js
+++ pages/theater.js
@@ -193,10 +193,6 @@ const [screenFactModes, setScreenFactModes] = useState(["off", "off", "off", "of
 
 const [zoomSyncOn, setZoomSyncOn] = useState(false);
 
-useEffect(() => {
-  console.log("THEATER CONTAINERS", theaterContainers);
-}, [theaterContainers]);
-
 function cycleScreenFactMode(screenIndex) {
   setScreenFactModes(current => {
     const next = [...current];
