b45c2bf60e394f91ea5e86031f20676f923a0505
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-24 00:56:31 -0500

    Display all machines in IXIPocket component
    Change the mapping of machines to display all instead of a maximum of seven.

--- components/ixi-chassis/IXIPocket.js
+++ components/ixi-chassis/IXIPocket.js
@@ -169,7 +169,7 @@ export default function IXIPocket({
 
       {pocketMode !== "closed" && hasMachines && (
         <div className={`ixi-pocket-thumbs thumb-size-${pocketThumbSize}`}>
-          {[...machines].reverse().slice(0, 7).map((machineId, index) => {
+          {[...machines].reverse().map((machineId, index) => {
             const machine = getListingById(machineId);
             if (!machine) return null;
 
