ea7810643006b484a5831f03124e9a6d5cbdaaac
tarpbarry-stack <tarpbarry@gmail.com>
2026-06-27 22:21:46 -0500

    Refactor moveMachineToContainer to use IXI_COMMANDS

--- pages/yard/index.js
+++ pages/yard/index.js
@@ -943,15 +943,12 @@ function executeIXITransaction(result) {
 function moveMachineToContainer(machineId, targetContainer) {
   if (!machineId || !targetContainer) return;
 
-  const result = executeIXIObjectTransaction({
-    type: IXI_TRANSACTION_TYPES.MOVE,
-    payload: {
-      objectId: machineId,
-      targetContainer,
-      ixiCardState,
-      machineContainers
-    }
-  });
+  const result = IXI_COMMANDS.moveObject({
+  objectId: machineId,
+  targetContainer,
+  ixiCardState,
+  machineContainers
+});
 
   executeIXITransaction(result);
 }
