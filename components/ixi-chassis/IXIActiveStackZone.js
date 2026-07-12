dcaeb1d88ffa3d0185c887d33708b13499cd904e
tarpbarry-stack <tarpbarry@gmail.com>
2026-07-11 22:38:36 -0500

    Update IXIActiveStackZone.js

--- components/ixi-chassis/IXIActiveStackZone.js
+++ components/ixi-chassis/IXIActiveStackZone.js
@@ -60,16 +60,14 @@ export default function IXIActiveStackZone({
           >
             <div className="active-stack-pocket-corners">
               <button
-                type="button"
-                className={`stack-pocket-power top-left ${
-                  armedDestination === "pocketLeft"
-                    ? "destination-armed"
-                    : ""
-                }`}
-                data-label="L1"
-                title="Arm L1"
-                onClick={() => toggleArmedDestination("pocketLeft")}
-              />
+  type="button"
+  className="stack-pocket-power top-left"
+  data-label="L1"
+  title="Send stack to L1"
+  onClick={() =>
+    moveActiveStackToContainer(stackKey, "pocketLeft")
+  }
+/>
 
               <button
                 type="button"
@@ -143,18 +141,14 @@ export default function IXIActiveStackZone({
             {activeStackSendMenu === stackKey && (
               <div className="active-stack-send-menu">
                 <button
-                  type="button"
-                  className={`stack-pocket-power top-left ${
-                    armedDestination === "pocketLeft"
-                      ? "destination-armed"
-                      : ""
-                  }`}
-                  data-label="L1"
-                  title="Arm L1"
-                  onClick={() => {
-                    toggleArmedDestination("pocketLeft");
-                  }}
-                />
+  type="button"
+  className="stack-send-option"
+  data-label="L1"
+  title="Send stack to L1"
+  onClick={() =>
+    moveActiveStackToContainer(stackKey, "pocketLeft")
+  }
+/>
 
                 <button
                   type="button"
