cc75c34c19c9d23820ed93635aafdbe3d76a1aae
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-02 12:44:13 -0500

    Adjust top position of IXIObjectCardActuator

--- components/ixi-chassis/IXIObjectCardActuator.jsx
+++ components/ixi-chassis/IXIObjectCardActuator.jsx
@@ -32,7 +32,7 @@ export default function IXIObjectCardActuator({
            * aligned with the price/state area,
            * below the photo and away from drag controls.
            */
-          top: 356px;
+          top: 396px;
 
           width: 5px;
           height: 34px;
