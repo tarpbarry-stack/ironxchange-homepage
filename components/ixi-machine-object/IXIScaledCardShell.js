0cc1c4370bc31d2e95f4f8ebd5c48b3288972bdc
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-02 13:55:15 -0500

    Update IXIScaledCardShell.js

--- components/ixi-machine-object/IXIScaledCardShell.js
+++ components/ixi-machine-object/IXIScaledCardShell.js
@@ -1,23 +1,57 @@
-import { getIXICardScalePreset } from "../../lib/ixiCardScalePresets";
+import {
+  getIXICardScalePreset
+} from "../../lib/ixiCardScalePresets";
 
-export default function IXIScaledCardShell({ size = "xl", children }) {
-  const metrics = getIXICardScalePreset(size);
+export default function IXIScaledCardShell({
+  size = "xl",
+  children,
+
+  nativeWidth = 298,
+  nativeHeight = 391,
+
+  tight = false
+}) {
+  const metrics =
+    getIXICardScalePreset(size);
+
+  const scale =
+    Number(metrics?.scale) || 1;
+
+  const footprintWidth =
+    tight
+      ? nativeWidth * scale
+      : metrics.width;
+
+  const footprintHeight =
+    tight
+      ? nativeHeight * scale
+      : metrics.height;
 
   return (
     <div
       className="ixi-scaled-card-shell"
       style={{
-        width: `${metrics.width}px`,
-        height: `${metrics.height}px`
+        width:
+          `${footprintWidth}px`,
+
+        height:
+          `${footprintHeight}px`
       }}
     >
       <div
         className="ixi-scaled-card-inner"
         style={{
-          width: "298px",
-          height: "391px",
-          transform: `scale(${metrics.scale})`,
-          transformOrigin: "top left"
+          width:
+            `${nativeWidth}px`,
+
+          height:
+            `${nativeHeight}px`,
+
+          transform:
+            `scale(${scale})`,
+
+          transformOrigin:
+            "top left"
         }}
       >
         {children}
@@ -26,14 +60,19 @@ export default function IXIScaledCardShell({ size = "xl", children }) {
       <style jsx>{`
         .ixi-scaled-card-shell {
           position: relative;
+
           flex: 0 0 auto;
+
           overflow: visible;
         }
 
         .ixi-scaled-card-inner {
           position: absolute;
+
           top: 0;
           left: 0;
+
+          overflow: visible;
         }
       `}</style>
     </div>
