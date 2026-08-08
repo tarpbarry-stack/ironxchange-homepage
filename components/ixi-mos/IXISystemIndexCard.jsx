0cfb027d9e0cb7fb2c1dcfdb45ab94da8d6f4fb3
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-08 16:52:39 -0500

    Adjust dimensions of system index card
    Updated the dimensions of the system index card.

--- components/ixi-mos/IXISystemIndexCard.jsx
+++ components/ixi-mos/IXISystemIndexCard.jsx
@@ -1060,15 +1060,15 @@ export default function IXISystemIndexCard({
            =============================================== */
 
         .system-index-card {
-          position: relative;
+  position: relative;
 
-          width: 298px;
-          min-width: 298px;
-          max-width: 298px;
+  width: 298px;
+  min-width: 298px;
+  max-width: 298px;
 
-          height: 391px;
-          min-height: 391px;
-          max-height: 391px;
+  height: 471px;
+  min-height: 471px;
+  max-height: 471px;
 
           overflow: hidden;
 
