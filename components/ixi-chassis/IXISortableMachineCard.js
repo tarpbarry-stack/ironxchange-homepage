e1053092cff0626ddd3dbf44529c3668ec2dc920
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-08 17:18:54 -0500

    Refactor IXISortableMachineCard to use IXISortableObject

--- components/ixi-chassis/IXISortableMachineCard.js
+++ components/ixi-chassis/IXISortableMachineCard.js
@@ -1,53 +1,39 @@
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import IXISortableObject
+  from "./IXISortableObject";
 
 export default function IXISortableMachineCard({
-  id,
-  containerId,
-  className,
-  style: externalStyle,
-  children
+  objectType = "machine",
+  objectFamily = "machine",
+  dragData = {},
+  ...props
 }) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    setActivatorNodeRef,
-    transform,
-    transition,
-    isDragging
-  } = useSortable({
-    id: String(id),
-    data: {
-      type: "machine",
-      containerId
-    }
-  });
-
-  const style = {
-    ...(externalStyle || {}),
-    transform: CSS.Transform.toString(transform),
-    transition,
-    opacity: isDragging ? 0 : 1,
-    zIndex: isDragging ? 9999 : externalStyle?.zIndex
-  };
-
   return (
-    <div
-      ref={setNodeRef}
-      className={className}
-      style={style}
-      data-ixi-sortable-card={String(id)}
-      data-ixi-container={containerId}
-    >
-      {children({
-        dragHandleProps: {
-          ref: setActivatorNodeRef,
-          ...attributes,
-          ...listeners
-        },
-        isDragging
-      })}
-    </div>
+    <IXISortableObject
+      {...props}
+
+      objectType={
+        objectType ||
+        "machine"
+      }
+
+      objectFamily={
+        objectFamily ||
+        "machine"
+      }
+
+      dragData={{
+        /*
+         * Compatibility marker.
+         * Existing pages may still
+         * identify these as legacy
+         * machine-sortable objects.
+         */
+        legacySortableMachine:
+          objectType ===
+          "machine",
+
+        ...dragData
+      }}
+    />
   );
 }
