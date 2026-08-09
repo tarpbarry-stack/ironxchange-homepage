d4cb9ef8926846d78ad05d6fd0f7d902b31d2008
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-09 10:01:05 -0500

    Change background and outline styles to transparent

--- components/ixi-chassis/IXIPocketL2.js
+++ components/ixi-chassis/IXIPocketL2.js
@@ -38,8 +38,8 @@ export default function IXIPocketL2({
     height: "calc(100vh - 420px)",
     pointerEvents: "none",
     zIndex: 999,
-    background: "rgba(0, 120, 255, .45)",
-    outline: "3px solid #0078ff"
+    background: "transparent",
+outline: "none"
   }}
 />
 
