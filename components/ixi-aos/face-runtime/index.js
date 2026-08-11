120abf76ad69933a3be138b6ea72b5ed8a11afed
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-11 00:16:18 -0500

    Add index.js for IXIAos face components exports

---
+++ components/ixi-aos/face-runtime/index.js
@@ -0,0 +1,51 @@
+export {
+  default as IXIAosFaceFrame
+} from "./IXIAosFaceFrame";
+
+export {
+  default as IXIAosFaceRow
+} from "./IXIAosFaceRow";
+
+export {
+  default as IXIAosFaceSection
+} from "./IXIAosFaceSection";
+
+export {
+  default as IXIAosFaceGrid
+} from "./IXIAosFaceGrid";
+
+export {
+  default as IXIAosFaceSummary
+} from "./IXIAosFaceSummary";
+
+export {
+  default as IXIAosFaceNotes
+} from "./IXIAosFaceNotes";
+
+export {
+  default as IXIAosFaceActionFooter
+} from "./IXIAosFaceActionFooter";
+
+export {
+  default as IXIAosFaceReferenceOverlay
+} from "./IXIAosFaceReferenceOverlay";
+
+export {
+  default as IXIAosRelationshipList
+} from "./IXIAosRelationshipList";
+
+export {
+  default as IXIAosFaceModuleRuntime
+} from "./IXIAosFaceModuleRuntime";
+
+export {
+  default as IXIAosFaceRuntime
+} from "./IXIAosFaceRuntime";
+
+export {
+  getIXIAosFaceId,
+  getIXIAosFaceNumber,
+  normalizeIXIAosFaceDefinition,
+  getIXIAosFaceDefinitions,
+  getIXIAosFaceDefinition
+} from "./IXIAosFaceDefinitionEngine";
