3cb26b45f5ee784ad824a1ef6c62289deb524144
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-14 18:41:41 -0500

    Add IXIAosCardPreview component

---
+++ components/ixi-face-studio/IXIAosCardPreview.jsx
@@ -0,0 +1,34 @@
+export default function IXIAosCardPreview({
+  children
+}) {
+  return (
+    <div className="ixi-aos-card-preview">
+      {children}
+
+      <style jsx>{`
+        .ixi-aos-card-preview {
+          box-sizing: border-box;
+
+          position: relative;
+
+          width: 298px;
+          min-width: 298px;
+          max-width: 298px;
+
+          height: 471px;
+          min-height: 471px;
+          max-height: 471px;
+
+          overflow: hidden;
+        }
+
+        .ixi-aos-card-preview > :global(*) {
+          width: 100%;
+          height: 100%;
+          min-height: 100%;
+          max-height: 100%;
+        }
+      `}</style>
+    </div>
+  );
+}
