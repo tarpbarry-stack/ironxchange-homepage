0201a59dabdcd054893ea2509b39b95324c4e2ff
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-14 21:54:35 -0500

    Add sample data for machines, vehicles, and tools
    Added sample data for vehicles and tools to IXIAosCardSampleData.js.

--- components/ixi-aos-card-library/IXIAosCardSampleData.js
+++ components/ixi-aos-card-library/IXIAosCardSampleData.js
@@ -66,6 +66,114 @@ const SAMPLE_LOCATION_ITEMS =
       objectId:
         "preview-machine-001",
 
+      entityId:
+        "aos-card-preview-entity",
+
+      objectType:
+        "machine",
+
+      displayName:
+        "2022 CATERPILLAR 336",
+
+      directContainerId:
+        "aos-card-preview-location-001",
+
+      value:
+        285000,
+
+      currency:
+        "USD",
+
+      fields: {
+        year:
+          2022,
+
+        make:
+          "CATERPILLAR",
+
+        model:
+          "336",
+
+        hours:
+          3842
+      },
+
+      media: [],
+
+      metadata: {}
+    },
+
+    {
+      objectId:
+        "preview-vehicle-001",
+
+      entityId:
+        "aos-card-preview-entity",
+
+      objectType:
+        "vehicle",
+
+      displayName:
+        "2024 FORD F-350",
+
+      directContainerId:
+        "aos-card-preview-location-001",
+
+      value:
+        72000,
+
+      currency:
+        "USD",
+
+      fields: {
+        year:
+          2024,
+
+        make:
+          "FORD",
+
+        model:
+          "F-350"
+      },
+
+      media: [],
+
+      metadata: {}
+    },
+
+    {
+      objectId:
+        "preview-tool-001",
+
+      entityId:
+        "aos-card-preview-entity",
+
+      objectType:
+        "tool",
+
+      displayName:
+        "FIELD TOOL SET",
+
+      directContainerId:
+        "aos-card-preview-location-001",
+
+      value:
+        8500,
+
+      currency:
+        "USD",
+
+      fields: {},
+
+      media: [],
+
+      metadata: {}
+    }
+  ]);
+    {
+      objectId:
+        "preview-machine-001",
+
       objectType:
         "machine"
     },
