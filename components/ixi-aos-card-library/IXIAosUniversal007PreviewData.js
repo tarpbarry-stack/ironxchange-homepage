bb5aadcfd7957cf4d17f315eb748f05b30602b4f
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 12:01:56 -0500

    Add universal 007 preview photo strip samples

---
+++ components/ixi-aos-card-library/IXIAosUniversal007PreviewData.js
@@ -0,0 +1,64 @@
+export function getUniversal007PreviewItems() {
+  return [
+    {
+      objectId: "u007-preview-child-001",
+      entityId: "aos-card-preview-entity",
+      objectType: "customer-defined-object",
+      singularLabel: "OBJECT",
+      displayName: "DEERE 85G",
+      fields: {},
+      fieldDefinitions: [],
+      relationships: [],
+      media: [{ url: "/images/2017-deere-85g.jpg" }],
+      metadata: { previewOnly: true }
+    },
+    {
+      objectId: "u007-preview-child-002",
+      entityId: "aos-card-preview-entity",
+      objectType: "customer-defined-object",
+      singularLabel: "OBJECT",
+      displayName: "BELL B30E",
+      fields: {},
+      fieldDefinitions: [],
+      relationships: [],
+      media: [{ url: "/images/2018-bell-b30e.jpg" }],
+      metadata: { previewOnly: true }
+    },
+    {
+      objectId: "u007-preview-child-003",
+      entityId: "aos-card-preview-entity",
+      objectType: "customer-defined-object",
+      singularLabel: "OBJECT",
+      displayName: "DEERE 750L",
+      fields: {},
+      fieldDefinitions: [],
+      relationships: [],
+      media: [{ url: "/images/2021-deere-750l.jpg" }],
+      metadata: { previewOnly: true }
+    },
+    {
+      objectId: "u007-preview-child-004",
+      entityId: "aos-card-preview-entity",
+      objectType: "customer-defined-object",
+      singularLabel: "OBJECT",
+      displayName: "KOMATSU WA475-10",
+      fields: {},
+      fieldDefinitions: [],
+      relationships: [],
+      media: [{ url: "/images/2023-komatsu-wa475-10.jpg" }],
+      metadata: { previewOnly: true }
+    },
+    {
+      objectId: "u007-preview-child-005",
+      entityId: "aos-card-preview-entity",
+      objectType: "customer-defined-object",
+      singularLabel: "OBJECT",
+      displayName: "PETERBILT 389",
+      fields: {},
+      fieldDefinitions: [],
+      relationships: [],
+      media: [{ url: "/images/2007-peterbilt-389.jpg" }],
+      metadata: { previewOnly: true }
+    }
+  ];
+}
