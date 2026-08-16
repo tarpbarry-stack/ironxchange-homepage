90cd22b426559972d306502d95e2bb7c1328db0e
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 01:04:06 -0500

    Make card 006 a real V12 personnel application

--- components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
+++ components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
@@ -1,3 +1,14 @@
-import IXIAosPersonnelContainerCard from "../personnel/IXIAosPersonnelContainerCard";
-export const AOS_CARD_006_PERSONNEL=Object.freeze({cardNumber:6,cardId:"006-personnel-v13",templateSlug:"personnel-container-006",label:"Employees / Personnel",version:13,variant:"dashboard"});
-export default function IXIAosCard006Personnel(props){return <IXIAosPersonnelContainerCard {...props} variant={3}/>;}
+import IXIAosPersonnelContainerApplication from "../personnel/IXIAosPersonnelContainerApplication";
+
+export const AOS_CARD_006_PERSONNEL = Object.freeze({
+  cardNumber: 6,
+  cardId: "006-personnel-v12",
+  templateSlug: "personnel-container-006",
+  label: "Employees / Personnel",
+  version: 12,
+  variant: "dashboard"
+});
+
+export default function IXIAosCard006Personnel(props) {
+  return <IXIAosPersonnelContainerApplication {...props} variant={3} />;
+}
