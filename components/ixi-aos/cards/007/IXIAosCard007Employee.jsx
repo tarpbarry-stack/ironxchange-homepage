7f52e5db6095cec0e22d1f7829bd8444c63bbd3d
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 11:42:52 -0500

    Apply corrected 4:3 media frame to universal 007 compatibility entry

--- components/ixi-aos/cards/007/IXIAosCard007Employee.jsx
+++ components/ixi-aos/cards/007/IXIAosCard007Employee.jsx
@@ -1,9 +1,15 @@
 import IXIAosGenericUniversalLayout007 from "../generic/IXIAosGenericUniversalLayout007";
+import IXIAosCard007MediaGeometry from "./IXIAosCard007MediaGeometry";
 
 /*
  * Compatibility export: older imports still reference this file name.
  * Card 007 itself is now the universal, noun-agnostic AOS card.
  */
 export default function IXIAosCard007Employee(props) {
-  return <IXIAosGenericUniversalLayout007 {...props} />;
+  return (
+    <>
+      <IXIAosGenericUniversalLayout007 {...props} />
+      <IXIAosCard007MediaGeometry />
+    </>
+  );
 }
