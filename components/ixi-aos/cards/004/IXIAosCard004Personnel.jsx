ef3cec53f4888b4cca7036a9265bb90343fa7c94
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 12:17:31 -0500

    Use V12 micro scrollbars on Card 004

--- components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
+++ components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
@@ -1,5 +1,6 @@
 import IXIAosGenericContainerLayoutV12 from "../generic/IXIAosGenericContainerLayoutV12";
 import IXIAosGenericCardRailShell from "../generic/IXIAosGenericCardRailShell";
+import IXIAosV12CardPolish from "../../card-runtime/modules/IXIAosV12CardPolish";
 
 export const AOS_CARD_004_PERSONNEL = Object.freeze({
   cardNumber: 4,
@@ -12,8 +13,11 @@ export const AOS_CARD_004_PERSONNEL = Object.freeze({
 
 export default function IXIAosCard004Personnel(props) {
   return (
-    <IXIAosGenericCardRailShell object={props.object} {...props} face={1}>
-      <IXIAosGenericContainerLayoutV12 {...props} variant={1} />
-    </IXIAosGenericCardRailShell>
+    <>
+      <IXIAosGenericCardRailShell object={props.object} {...props} face={1}>
+        <IXIAosGenericContainerLayoutV12 {...props} variant={1} />
+      </IXIAosGenericCardRailShell>
+      <IXIAosV12CardPolish />
+    </>
   );
 }
