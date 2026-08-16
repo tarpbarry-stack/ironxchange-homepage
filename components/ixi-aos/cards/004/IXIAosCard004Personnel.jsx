ab1f385994081a01592b4ed19ba461c98a9ff20e
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 00:01:34 -0500

    Add AOS card 004 personnel container

---
+++ components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
@@ -0,0 +1,3 @@
+import IXIAosPersonnelContainerCard from "../personnel/IXIAosPersonnelContainerCard";
+export const AOS_CARD_004_PERSONNEL=Object.freeze({cardNumber:4,cardId:"004-personnel-v13",templateSlug:"personnel-container-004",label:"Employees / Personnel",version:13,variant:"summary"});
+export default function IXIAosCard004Personnel(props){return <IXIAosPersonnelContainerCard {...props} variant={1}/>;}
