122d593cdbb6e0e8c9b4c222b8d987a918266aa7
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 01:40:18 -0500

    Route card 006 through generic schema-driven container layout

--- components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
+++ components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
@@ -1,14 +1,14 @@
-import IXIAosPersonnelContainerApplication from "../personnel/IXIAosPersonnelContainerApplication";
+import IXIAosGenericContainerLayoutV12 from "../generic/IXIAosGenericContainerLayoutV12";
 
 export const AOS_CARD_006_PERSONNEL = Object.freeze({
   cardNumber: 6,
-  cardId: "006-personnel-v12",
+  cardId: "006-generic-container-v12",
   templateSlug: "personnel-container-006",
-  label: "Employees / Personnel",
+  label: "Container Layout 006",
   version: 12,
   variant: "dashboard"
 });
 
 export default function IXIAosCard006Personnel(props) {
-  return <IXIAosPersonnelContainerApplication {...props} variant={3} />;
+  return <IXIAosGenericContainerLayoutV12 {...props} variant={3} />;
 }
