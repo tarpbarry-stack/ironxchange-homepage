7dca6ceca2ba68a7d5e638a4e5943006b1e1a4ff
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 01:02:07 -0500

    Route card 004 to standalone functional V12 implementation

--- components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
+++ components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
@@ -1,3 +1,14 @@
-import IXIAosPersonnelContainerCard from "../personnel/IXIAosPersonnelContainerCard";
-export const AOS_CARD_004_PERSONNEL=Object.freeze({cardNumber:4,cardId:"004-personnel-v13",templateSlug:"personnel-container-004",label:"Employees / Personnel",version:13,variant:"summary"});
-export default function IXIAosCard004Personnel(props){return <IXIAosPersonnelContainerCard {...props} variant={1}/>;}
+import IXIAosCard004PersonnelV12 from "./IXIAosCard004PersonnelV12";
+
+export const AOS_CARD_004_PERSONNEL = Object.freeze({
+  cardNumber: 4,
+  cardId: "004-personnel-v12",
+  templateSlug: "personnel-container-004",
+  label: "Employees / Personnel",
+  version: 12,
+  variant: "summary"
+});
+
+export default function IXIAosCard004Personnel(props) {
+  return <IXIAosCard004PersonnelV12 {...props} />;
+}
