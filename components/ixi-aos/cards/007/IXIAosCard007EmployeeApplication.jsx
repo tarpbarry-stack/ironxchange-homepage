f6a6a4c4f2480a1098b243f27400d6855b4f7721
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 11:42:38 -0500

    Apply corrected 4:3 media frame to universal 007 card

--- components/ixi-aos/cards/007/IXIAosCard007EmployeeApplication.jsx
+++ components/ixi-aos/cards/007/IXIAosCard007EmployeeApplication.jsx
@@ -1,9 +1,15 @@
 import IXIAosGenericUniversalLayout007 from "../generic/IXIAosGenericUniversalLayout007";
+import IXIAosCard007MediaGeometry from "./IXIAosCard007MediaGeometry";
 
 /*
  * Compatibility export for existing Face Lab/runtime imports.
  * Card 007 is now the universal AOS card.
  */
 export default function IXIAosCard007EmployeeApplication(props) {
-  return <IXIAosGenericUniversalLayout007 {...props} />;
+  return (
+    <>
+      <IXIAosGenericUniversalLayout007 {...props} />
+      <IXIAosCard007MediaGeometry />
+    </>
+  );
 }
