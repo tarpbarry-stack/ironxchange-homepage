744a8d3642d7159ca31c58d728490b9ce9210a10
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 17:36:26 -0500

    Add Access Policy module metadata

---
+++ components/ixi-aos/transact/modules/access-policy/IXIAccessPolicyModuleMeta.js
@@ -0,0 +1,8 @@
+export const IXI_ACCESS_POLICY_MODULE = Object.freeze({
+  id: "access-policy",
+  label: "ACCESS / POLICY",
+  group: "security",
+  documentType: "authority-policy"
+});
+
+export default IXI_ACCESS_POLICY_MODULE;
