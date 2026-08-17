5d1799ac7e4ea733bf88c4ec8b7ded7fd150bdf2
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 20:34:31 -0500

    Add card 011 runtime wrapper

---
+++ components/ixi-aos/cards/011/IXIAosCard011.jsx
@@ -0,0 +1,20 @@
+import IXIAosGenericMetricDominant011 from "../generic/IXIAosGenericMetricDominant011";
+import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
+
+export const CARD_011 = Object.freeze({
+  cardNumber: 11,
+  templateSlug: "aos-card-011",
+  nativeWidth: 298,
+  nativeHeight: 471,
+  railReserve: 23,
+  version: 12,
+  renderer: "schema-driven-generic"
+});
+
+export default function IXIAosCard011(props) {
+  return (
+    <IXIAosDataContractCardAdapter {...props} minimumCustomFields={9}>
+      {contractProps => <IXIAosGenericMetricDominant011 {...contractProps} />}
+    </IXIAosDataContractCardAdapter>
+  );
+}
