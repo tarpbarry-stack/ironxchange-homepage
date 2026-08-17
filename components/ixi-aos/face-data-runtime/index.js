04044a8dc4bd1614d78ba8053261520dae3f772e
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 00:45:15 -0500

    Export Face data runtime

---
+++ components/ixi-aos/face-data-runtime/index.js
@@ -0,0 +1,12 @@
+export {
+  IXI_AUTHORIZED_FACE_DATA_MANIFEST_SCHEMA,
+  createIXIAuthorizedFaceDataManifest,
+  getIXIAuthorizedFaceCapabilityIds,
+  hasIXIAuthorizedFaceCapability
+} from "./IXIAuthorizedFaceDataManifest";
+
+export {
+  IXI_FACE_APP_AUTHORIZATION_SCHEMA,
+  authorizeIXIFaceAppDefinition,
+  assertIXIFaceAppAuthorized
+} from "./IXIFaceAppAuthorizationEngine";
