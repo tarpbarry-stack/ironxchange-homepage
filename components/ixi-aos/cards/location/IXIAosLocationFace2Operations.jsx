237334ee627c0d0226a22d886815e83370ce15d0
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 01:19:14 -0500

    Restore specialized V12 location Face 2 runtime

--- components/ixi-aos/cards/location/IXIAosLocationFace2Operations.jsx
+++ components/ixi-aos/cards/location/IXIAosLocationFace2Operations.jsx
@@ -1,25 +1,17 @@
-import IXIAosGenericConfiguredFaceV12 from "../generic/IXIAosGenericConfiguredFaceV12";
+import IXIAosLocationFace2OperationsV12, {
+  LOCATION_FACE2_SKINS,
+  LOCATION_FACE2_V12_LOCK
+} from "./IXIAosLocationFace2OperationsV12";
 
-export const LOCATION_FACE2_SKINS = Object.freeze([
-  Object.freeze({ id: "v12", label: "V12" }),
-  Object.freeze({ id: "steel", label: "STEEL" }),
-  Object.freeze({ id: "blueprint", label: "BLUE" }),
-  Object.freeze({ id: "industrial", label: "INDUSTRIAL" })
-]);
+export { LOCATION_FACE2_SKINS };
 
 export const LOCATION_FACE2_GENERIC_LOCK = Object.freeze({
-  face: 2,
-  version: 12,
-  nativeWidth: 298,
-  nativeHeight: 471,
-  renderer: "schema-driven-generic"
+  ...LOCATION_FACE2_V12_LOCK,
+  renderer: "specialized-v12-location-operations",
+  singleScrollSurface: true,
+  allOperationalFieldsEditable: true
 });
 
 export default function IXIAosLocationFace2Operations(props) {
-  return (
-    <IXIAosGenericConfiguredFaceV12
-      {...props}
-      faceNumber={2}
-    />
-  );
+  return <IXIAosLocationFace2OperationsV12 {...props} />;
 }
