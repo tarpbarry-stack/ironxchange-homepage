0386451cc5fc7248d34018ec520082b125160a1d
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 01:28:49 -0500

    Register restored V12 location operations Face 2

--- components/ixi-face-studio/IXIAosFaceRendererRegistry.jsx
+++ components/ixi-face-studio/IXIAosFaceRendererRegistry.jsx
@@ -1,10 +1,22 @@
 import IXILocationObjectFace1
   from "../ixi-mos/location/IXILocationObjectFace1";
 
+import IXIAosLocationFace2Operations
+  from "../ixi-aos/cards/location/IXIAosLocationFace2Operations";
+
 
 const AOS_FACE_RENDERERS = Object.freeze({
   "location-face-1":
-    IXILocationObjectFace1
+    IXILocationObjectFace1,
+
+  "location-face-2":
+    IXIAosLocationFace2Operations,
+
+  "location-operations":
+    IXIAosLocationFace2Operations,
+
+  "location-operations-v12":
+    IXIAosLocationFace2Operations
 });
 
 
