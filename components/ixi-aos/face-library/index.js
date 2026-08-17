51026736088cadbb4c7a24191bdc58ed4028d003
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 00:51:35 -0500

    Export Company Face Library runtime

---
+++ components/ixi-aos/face-library/index.js
@@ -0,0 +1,7 @@
+export * from "./IXIFaceLibraryContract";
+export * from "./IXIFaceLibraryPolicyEngine";
+export * from "./IXIFaceLibraryVersionEngine";
+export * from "./IXIFaceLibraryAssignmentEngine";
+export * from "./IXIFaceLibraryValidationEngine";
+export * from "./IXIFaceLibraryRuntimeResolver";
+export * from "./IXIFaceLibraryClient";
