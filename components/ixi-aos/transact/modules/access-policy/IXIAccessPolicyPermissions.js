35deb8b06ae4b22fe17224f643c947a9b8f8ca99
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 17:39:09 -0500

    Add Access Policy permission helpers

---
+++ components/ixi-aos/transact/modules/access-policy/IXIAccessPolicyPermissions.js
@@ -0,0 +1,4 @@
+export function hasAuthorityManage(accessContext = {}) {
+  const grants = accessContext?.principal?.directGrants || accessContext?.capabilities?.directGrants || [];
+  return Array.isArray(grants) && grants.includes("authority.manage");
+}
