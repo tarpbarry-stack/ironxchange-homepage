7de51aa2d223addea93c6b78c34944482f974ab5
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-16 20:38:27 -0500

    Register Card 012 runtime

---
+++ components/ixi-aos/cards/012/IXIAosCard012.jsx
@@ -0,0 +1,20 @@
+import IXIAosGenericLifecycle012 from "../generic/IXIAosGenericLifecycle012";
+import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
+
+export const CARD_012 = Object.freeze({
+  cardNumber: 12,
+  templateSlug: "aos-card-012",
+  nativeWidth: 298,
+  nativeHeight: 471,
+  railReserve: 23,
+  version: 12,
+  renderer: "schema-driven-generic"
+});
+
+export default function IXIAosCard012(props) {
+  return (
+    <IXIAosDataContractCardAdapter {...props} minimumCustomFields={10}>
+      {contractProps => <IXIAosGenericLifecycle012 {...contractProps} />}
+    </IXIAosDataContractCardAdapter>
+  );
+}
