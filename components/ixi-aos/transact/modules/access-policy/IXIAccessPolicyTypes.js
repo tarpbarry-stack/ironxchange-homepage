70b5d0c3314226a32c1e165fcce62aefb2c56376
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 17:37:22 -0500

    Add Access Policy subject and scope types

---
+++ components/ixi-aos/transact/modules/access-policy/IXIAccessPolicyTypes.js
@@ -0,0 +1,2 @@
+export const IXI_ACCESS_POLICY_SUBJECT_TYPES = Object.freeze(["role", "principal", "group", "authenticated"]);
+export const IXI_ACCESS_POLICY_SCOPE_TYPES = Object.freeze(["target", "target-and-descendants"]);
