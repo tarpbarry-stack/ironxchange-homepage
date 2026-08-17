d2455e9f784e969b8a2d59e816f692f9b65a4cf9
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-17 17:35:21 -0500

    Add Access Policy capability constants

---
+++ components/ixi-aos/transact/modules/access-policy/IXIAccessPolicyConstants.js
@@ -0,0 +1,14 @@
+export const IXI_ACCESS_POLICY_CAPABILITIES = Object.freeze([
+  "aos.discover",
+  "aos.view",
+  "aos.edit",
+  "aos.move",
+  "aos.delete",
+  "authority.manage",
+  "identity.invite",
+  "work-order.view",
+  "work-order.time.create",
+  "work-order.material.create",
+  "transact.gl.view",
+  "transact.treasury.view"
+]);
