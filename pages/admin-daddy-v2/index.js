f4dbb4d9f906a2bf7922a1bf8b893fcc6bf7dbbd
tarpbarry-stack <tarpbarry@gmail.com>
2026-08-18 00:34:22 -0500

    Add Admin Daddy V2 route

---
+++ pages/admin-daddy-v2/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import AdminDaddyApp from "../../components/admin-daddy-v2/AdminDaddyApp";
+
+export default function AdminDaddyV2Page() {
+  return (
+    <>
+      <Head>
+        <title>Admin Daddy II | IronXchange</title>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+      <AdminDaddyApp />
+    </>
+  );
+}
