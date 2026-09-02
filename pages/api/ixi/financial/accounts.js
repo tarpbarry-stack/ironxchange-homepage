3514ecaa7983ae62eb820f3248a4628b838caaa0
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-01 21:19:28 -0500

    Add TRAN$ACT chart of accounts proxy

---
+++ pages/api/ixi/financial/accounts.js
@@ -0,0 +1,24 @@
+const {
+  proxyIXIFinancialRequest
+} = require("../../../../lib/ixi-financial/ixiFinancialProxy");
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", "GET");
+    return res.status(405).json({
+      ok: false,
+      error: {
+        code: "METHOD_NOT_ALLOWED",
+        message: "GET required."
+      }
+    });
+  }
+
+  return proxyIXIFinancialRequest({
+    req,
+    res,
+    path: "/financial/accounts",
+    method: "GET",
+    timeoutMs: 30000
+  });
+}
