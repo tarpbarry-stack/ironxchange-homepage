8549e2d2eddf1af9829b0aa8f3741cf193e2f192
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-01 21:17:08 -0500

    Wire mobile incident command into Admin Daddy

--- pages/admin-daddy-v2/index.js
+++ pages/admin-daddy-v2/index.js
@@ -3,6 +3,7 @@ import AdminDaddyApp from "../../components/admin-daddy-v2/AdminDaddyApp";
 import AdminDaddyTaskCenter from "../../components/admin-daddy-v2/AdminDaddyTaskCenter";
 import AdminDaddyOperationsDeck from "../../components/admin-daddy-v2/AdminDaddyOperationsDeck";
 import AdminDaddyEnterpriseDeck from "../../components/admin-daddy-v2/AdminDaddyEnterpriseDeck";
+import AdminDaddyMobileCommand from "../../components/admin-daddy-v2/AdminDaddyMobileCommand";
 
 export default function AdminDaddyV2Page() {
   return (
@@ -10,7 +11,9 @@ export default function AdminDaddyV2Page() {
       <Head>
         <title>Admin Daddy II | IronXchange</title>
         <meta name="robots" content="noindex,nofollow" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
       </Head>
+      <AdminDaddyMobileCommand />
       <AdminDaddyApp />
       <AdminDaddyTaskCenter />
       <AdminDaddyOperationsDeck />
