5b0bb33c9386f9680bc317bbca90da9d0d97abc9
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-03 02:59:24 -0500

    Route Card 004 to native Face 1 edit and V12 readability

--- components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
+++ components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
@@ -2,7 +2,8 @@ import IXIAosGenericContainerLayoutV12 from "../generic/IXIAosGenericContainerLa
 import IXIAosGenericCardRailShell from "../generic/IXIAosGenericCardRailShell";
 import IXIAosV12CardPolish from "../../card-runtime/modules/IXIAosV12CardPolish";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
-import IXIAosCommercialEditorBridge from "../../card-runtime/modules/IXIAosCommercialEditorBridge";
+import IXIAosV12LibraryReadability from "../../card-runtime/modules/IXIAosV12LibraryReadability";
+import IXIAosV12Face1EditPatch from "../../card-runtime/modules/IXIAosV12Face1EditPatch";
 
 export const AOS_CARD_004_PERSONNEL = Object.freeze({
   cardNumber: 4,
@@ -17,16 +18,14 @@ export default function IXIAosCard004Personnel(props) {
   return (
     <IXIAosDataContractCardAdapter {...props}>
       {contractProps => (
-        <IXIAosCommercialEditorBridge object={contractProps.object} onSaveObject={contractProps.onSaveObject}>
-          {({ object }) => (
-            <>
-              <IXIAosGenericCardRailShell {...contractProps} object={object} face={1}>
-                <IXIAosGenericContainerLayoutV12 {...contractProps} object={object} variant={1} />
-              </IXIAosGenericCardRailShell>
-              <IXIAosV12CardPolish />
-            </>
-          )}
-        </IXIAosCommercialEditorBridge>
+        <div className="ixi-v12-library-readable ixi-v12-face1-edit" style={{ width: 298, height: 471 }}>
+          <IXIAosGenericCardRailShell {...contractProps} object={contractProps.object} face={1}>
+            <IXIAosGenericContainerLayoutV12 {...contractProps} object={contractProps.object} variant={1} />
+          </IXIAosGenericCardRailShell>
+          <IXIAosV12CardPolish />
+          <IXIAosV12LibraryReadability />
+          <IXIAosV12Face1EditPatch />
+        </div>
       )}
     </IXIAosDataContractCardAdapter>
   );
