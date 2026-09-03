cfba240e8fdf8b6dca019b895f9f245f9b66d173
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-02 22:29:15 -0500

    Adopt commercial editor on Card 006

--- components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
+++ components/ixi-aos/cards/006/IXIAosCard006Personnel.jsx
@@ -2,6 +2,7 @@ import IXIAosGenericContainerLayoutV12 from "../generic/IXIAosGenericContainerLa
 import IXIAosGenericCardRailShell from "../generic/IXIAosGenericCardRailShell";
 import IXIAosV12CardPolish from "../../card-runtime/modules/IXIAosV12CardPolish";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
+import IXIAosCommercialEditorBridge from "../../card-runtime/modules/IXIAosCommercialEditorBridge";
 
 export const AOS_CARD_006_PERSONNEL = Object.freeze({
   cardNumber: 6,
@@ -16,12 +17,16 @@ export default function IXIAosCard006Personnel(props) {
   return (
     <IXIAosDataContractCardAdapter {...props}>
       {contractProps => (
-        <>
-          <IXIAosGenericCardRailShell object={contractProps.object} {...contractProps} face={1}>
-            <IXIAosGenericContainerLayoutV12 {...contractProps} variant={3} />
-          </IXIAosGenericCardRailShell>
-          <IXIAosV12CardPolish />
-        </>
+        <IXIAosCommercialEditorBridge object={contractProps.object} onSaveObject={contractProps.onSaveObject}>
+          {({ object }) => (
+            <>
+              <IXIAosGenericCardRailShell {...contractProps} object={object} face={1}>
+                <IXIAosGenericContainerLayoutV12 {...contractProps} object={object} variant={3} />
+              </IXIAosGenericCardRailShell>
+              <IXIAosV12CardPolish />
+            </>
+          )}
+        </IXIAosCommercialEditorBridge>
       )}
     </IXIAosDataContractCardAdapter>
   );
