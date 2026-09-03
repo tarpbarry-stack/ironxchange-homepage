444cc867b2b633d5b30fd6710885afb89ebc3a38
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-03 03:04:53 -0500

    Route Card 017 to Face 1 edit runtime

--- components/ixi-aos/cards/017/IXIAosCard017.jsx
+++ components/ixi-aos/cards/017/IXIAosCard017.jsx
@@ -1,6 +1,6 @@
 import IXIAosGenericStructuralContainer017 from "../generic/IXIAosGenericStructuralContainer017";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
-import IXIAosCommercialEditorBridge from "../../card-runtime/modules/IXIAosCommercialEditorBridge";
+import IXIAosFace1CardRuntime from "../../card-runtime/modules/IXIAosFace1CardRuntime";
 
 export const CARD_017 = Object.freeze({
   cardNumber: 17,
@@ -16,9 +16,9 @@ export default function IXIAosCard017(props) {
   return (
     <IXIAosDataContractCardAdapter {...props} minimumCustomFields={4}>
       {contractProps => (
-        <IXIAosCommercialEditorBridge {...contractProps} object={contractProps.object} minimumCustomFields={4} mediaEnabled>
-          {({ object: runtimeObject }) => <IXIAosGenericStructuralContainer017 {...contractProps} object={runtimeObject} />}
-        </IXIAosCommercialEditorBridge>
+        <IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={4}>
+          {face1 => <IXIAosGenericStructuralContainer017 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject} />}
+        </IXIAosFace1CardRuntime>
       )}
     </IXIAosDataContractCardAdapter>
   );
