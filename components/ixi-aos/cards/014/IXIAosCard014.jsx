3efb2bf435613e65daba570f762d61b4f6f3425e
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-03 11:58:39 -0500

    Add IXI header identity to Card 014

--- components/ixi-aos/cards/014/IXIAosCard014.jsx
+++ components/ixi-aos/cards/014/IXIAosCard014.jsx
@@ -1,25 +1,6 @@
 import IXIAosGenericCondition014 from "../generic/IXIAosGenericCondition014";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
 import IXIAosFace1CardRuntime from "../../card-runtime/modules/IXIAosFace1CardRuntime";
-
-export const CARD_014 = Object.freeze({
-  cardNumber: 14,
-  templateSlug: "aos-card-014",
-  nativeWidth: 298,
-  nativeHeight: 471,
-  railReserve: 23,
-  version: 12,
-  renderer: "schema-driven-generic"
-});
-
-export default function IXIAosCard014(props) {
-  return (
-    <IXIAosDataContractCardAdapter {...props} minimumCustomFields={10}>
-      {contractProps => (
-        <IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={10}>
-          {face1 => <IXIAosGenericCondition014 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject} />}
-        </IXIAosFace1CardRuntime>
-      )}
-    </IXIAosDataContractCardAdapter>
-  );
-}
+import IXIAosCardHeaderIdentity from "../../card-runtime/modules/IXIAosCardHeaderIdentity";
+export const CARD_014=Object.freeze({cardNumber:14,templateSlug:"aos-card-014",nativeWidth:298,nativeHeight:471,railReserve:23,version:12,renderer:"schema-driven-generic"});
+export default function IXIAosCard014(props){return <IXIAosDataContractCardAdapter {...props} minimumCustomFields={10}>{contractProps=><IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={10}>{face1=><IXIAosCardHeaderIdentity object={face1.object}><IXIAosGenericCondition014 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject}/></IXIAosCardHeaderIdentity>}</IXIAosFace1CardRuntime>}</IXIAosDataContractCardAdapter>}
