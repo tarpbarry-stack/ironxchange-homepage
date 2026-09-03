8fe285fa6740a71342dbbe84c3d79910a6ff164a
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-03 11:56:48 -0500

    Add IXI header identity to Card 004

--- components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
+++ components/ixi-aos/cards/004/IXIAosCard004Personnel.jsx
@@ -3,31 +3,8 @@ import IXIAosGenericCardRailShell from "../generic/IXIAosGenericCardRailShell";
 import IXIAosV12CardPolish from "../../card-runtime/modules/IXIAosV12CardPolish";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
 import IXIAosFace1CardRuntime from "../../card-runtime/modules/IXIAosFace1CardRuntime";
+import IXIAosCardHeaderIdentity from "../../card-runtime/modules/IXIAosCardHeaderIdentity";
 
-export const AOS_CARD_004_PERSONNEL = Object.freeze({
-  cardNumber: 4,
-  cardId: "004-generic-container-v12",
-  templateSlug: "personnel-container-004",
-  label: "Container Layout 004",
-  version: 12,
-  variant: "summary"
-});
+export const AOS_CARD_004_PERSONNEL = Object.freeze({cardNumber:4,cardId:"004-generic-container-v12",templateSlug:"personnel-container-004",label:"Container Layout 004",version:12,variant:"summary"});
 
-export default function IXIAosCard004Personnel(props) {
-  return (
-    <IXIAosDataContractCardAdapter {...props}>
-      {contractProps => (
-        <IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={0}>
-          {face1 => (
-            <>
-              <IXIAosGenericCardRailShell {...contractProps} object={face1.object} face={1}>
-                <IXIAosGenericContainerLayoutV12 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject} variant={1} />
-              </IXIAosGenericCardRailShell>
-              <IXIAosV12CardPolish />
-            </>
-          )}
-        </IXIAosFace1CardRuntime>
-      )}
-    </IXIAosDataContractCardAdapter>
-  );
-}
+export default function IXIAosCard004Personnel(props){return <IXIAosDataContractCardAdapter {...props}>{contractProps=><IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={0}>{face1=><IXIAosCardHeaderIdentity object={face1.object}><IXIAosGenericCardRailShell {...contractProps} object={face1.object} face={1}><IXIAosGenericContainerLayoutV12 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject} variant={1}/></IXIAosGenericCardRailShell><IXIAosV12CardPolish/></IXIAosCardHeaderIdentity>}</IXIAosFace1CardRuntime>}</IXIAosDataContractCardAdapter>}
