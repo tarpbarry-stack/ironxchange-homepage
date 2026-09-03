5269a820ce67fc546eb0e30657e1f9442be8294b
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-03 03:06:02 -0500

    Constrain Card 005 edit to visible Face 1 contract

--- components/ixi-aos/cards/005/IXIAosCard005Personnel.jsx
+++ components/ixi-aos/cards/005/IXIAosCard005Personnel.jsx
@@ -2,8 +2,7 @@ import IXIAosGenericContainerLayoutV12 from "../generic/IXIAosGenericContainerLa
 import IXIAosGenericCardRailShell from "../generic/IXIAosGenericCardRailShell";
 import IXIAosV12CardPolish from "../../card-runtime/modules/IXIAosV12CardPolish";
 import IXIAosDataContractCardAdapter from "../../card-runtime/IXIAosDataContractCardAdapter";
-import IXIAosV12LibraryReadability from "../../card-runtime/modules/IXIAosV12LibraryReadability";
-import IXIAosV12Face1EditPatch from "../../card-runtime/modules/IXIAosV12Face1EditPatch";
+import IXIAosFace1CardRuntime from "../../card-runtime/modules/IXIAosFace1CardRuntime";
 
 export const AOS_CARD_005_PERSONNEL = Object.freeze({
   cardNumber: 5,
@@ -18,14 +17,16 @@ export default function IXIAosCard005Personnel(props) {
   return (
     <IXIAosDataContractCardAdapter {...props}>
       {contractProps => (
-        <div className="ixi-v12-library-readable ixi-v12-face1-edit" style={{ width: 298, height: 471 }}>
-          <IXIAosGenericCardRailShell {...contractProps} object={contractProps.object} face={1}>
-            <IXIAosGenericContainerLayoutV12 {...contractProps} object={contractProps.object} variant={2} />
-          </IXIAosGenericCardRailShell>
-          <IXIAosV12CardPolish />
-          <IXIAosV12LibraryReadability />
-          <IXIAosV12Face1EditPatch />
-        </div>
+        <IXIAosFace1CardRuntime object={contractProps.object} onSaveObject={contractProps.onSaveObject} maxFields={0}>
+          {face1 => (
+            <>
+              <IXIAosGenericCardRailShell {...contractProps} object={face1.object} face={1}>
+                <IXIAosGenericContainerLayoutV12 {...contractProps} object={face1.object} onSaveObject={face1.onSaveObject} variant={2} />
+              </IXIAosGenericCardRailShell>
+              <IXIAosV12CardPolish />
+            </>
+          )}
+        </IXIAosFace1CardRuntime>
       )}
     </IXIAosDataContractCardAdapter>
   );
