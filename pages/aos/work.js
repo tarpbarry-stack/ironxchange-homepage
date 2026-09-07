35b534308ceea4124ce1d4b58c08499a254ce579
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-07 13:34:31 -0500

    Add IXI-120 Lift Envelope board skin

--- pages/aos/work.js
+++ pages/aos/work.js
@@ -4006,6 +4006,11 @@ onClearContainerToParent={
           background-image: url('/images/ixi-aos-board-ixi-119.webp');
         }
 
+        .aos-work-board-skin-ixi-120 {
+          background-color: #000;
+          background-image: url('/images/ixi-aos-board-ixi-120.webp');
+        }
+
         .aos-work-board-title {
           position: absolute;
           width: 1px;
