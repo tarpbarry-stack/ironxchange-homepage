bc4c01536b00eed144a65cf30678ce7648bf658c
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-06 23:09:43 -0500

    Add IXI-106 LOAD PATH board skin

--- components/ixi-aos/board-skin-runtime/IXIAosBoardSkinPicker.jsx
+++ components/ixi-aos/board-skin-runtime/IXIAosBoardSkinPicker.jsx
@@ -256,11 +256,18 @@ export default function IXIAosBoardSkinPicker({
           background-size: cover;
         }
 
+        .preview-ixi-106 {
+          background-image: url('/images/ixi-aos-board-ixi-106.webp');
+          background-position: center;
+          background-size: cover;
+        }
+
         .preview-ixi-101 b,
         .preview-ixi-102 b,
         .preview-ixi-103 b,
         .preview-ixi-104 b,
-        .preview-ixi-105 b {
+        .preview-ixi-105 b,
+        .preview-ixi-106 b {
           color: transparent;
           font-size: 1px;
         }
