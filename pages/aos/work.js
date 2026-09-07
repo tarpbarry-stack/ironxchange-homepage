c8b36d914d0dd1e39b1e0aa2922e90a0b84a90ee
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-07 01:23:47 -0500

    Add IXI-117 EXTENDAHOE and IXI-118 COMPACTION (#250)
    Deploy the approved Case 590SN 4×4 Extendahoe and HAMM 84-inch padfoot shell-kit backgrounds to the AOS Work skin library.

--- pages/aos/work.js
+++ pages/aos/work.js
@@ -3681,6 +3681,16 @@ onReturnContainerChildren={
           background-image: url('/images/ixi-aos-board-ixi-116.webp');
         }
 
+        .aos-work-board-skin-ixi-117 {
+          background-color: #000;
+          background-image: url('/images/ixi-aos-board-ixi-117.webp');
+        }
+
+        .aos-work-board-skin-ixi-118 {
+          background-color: #000;
+          background-image: url('/images/ixi-aos-board-ixi-118.webp');
+        }
+
         .aos-work-board-title {
           position: absolute;
           width: 1px;
