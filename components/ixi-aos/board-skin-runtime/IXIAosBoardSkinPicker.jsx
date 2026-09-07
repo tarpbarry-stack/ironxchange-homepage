3fc6caf3b56fc1fe0fd1c349c4914f355bc559e7
tarpbarry-stack <tarpbarry@gmail.com>
2026-09-06 22:59:56 -0500

    Move AOS skin labels below rail and add IXI-105

--- components/ixi-aos/board-skin-runtime/IXIAosBoardSkinPicker.jsx
+++ components/ixi-aos/board-skin-runtime/IXIAosBoardSkinPicker.jsx
@@ -250,10 +250,17 @@ export default function IXIAosBoardSkinPicker({
           background-size: cover;
         }
 
+        .preview-ixi-105 {
+          background-image: url('/images/ixi-aos-board-ixi-105.webp');
+          background-position: center;
+          background-size: cover;
+        }
+
         .preview-ixi-101 b,
         .preview-ixi-102 b,
         .preview-ixi-103 b,
-        .preview-ixi-104 b {
+        .preview-ixi-104 b,
+        .preview-ixi-105 b {
           color: transparent;
           font-size: 1px;
         }
